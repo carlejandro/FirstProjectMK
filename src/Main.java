8683558e75646d57358bb468dbb7d01204654716
carlejandro <xandersuarezm@gmail.com>
2023-06-28 14:18:03 -0400

    My first commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        //SWAP 1
+        byte numOne = 1;
+        byte numTwo = 2;
+        byte swap = numOne;
+        numOne = numTwo;
+        numTwo = swap;
+        System.out.println(numOne);
+        System.out.println(numTwo);
+
+        //SWAP 2
+        String firstString = "Mark";
+        String secondString = "Sup";
+        String stringSwap = firstString;
+        firstString = secondString;
+        secondString = stringSwap;
+        System.out.println(firstString);
+        System.out.println(secondString);
+
+        }
+    }
